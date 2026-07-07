ea202a75642734cf9f173cbb54c981fffa85bc5f
CasCadeVR <girmilend@gmail.com>
2026-07-07 18:24:31 +0300

    made models

---
+++ Core/Entities/LearnAInalytics.Entities/Enums/QuestionType.cs
@@ -0,0 +1,22 @@
+﻿namespace LearnAInalytics.Entities.Enums;
+
+/// <summary>
+/// Тип вопроса
+/// </summary>
+public enum QuestionType
+{
+    /// <summary>
+    /// баллы 1-10
+    /// </summary>
+    Numeric,
+
+    /// <summary>
+    /// да/нет
+    /// </summary>
+    Binary,
+
+    /// <summary>
+    /// открытый ответ
+    /// </summary>
+    OpenText
+}
---
+++ Core/Entities/LearnAInalytics.Entities/Models/SurveyQuestion.cs
@@ -0,0 +1,24 @@
+﻿using LearnAInalytics.Entities.Enums;
+
+namespace LearnAInalytics.Entities.Models;
+
+/// <summary>
+/// Вопрос анкеты
+/// </summary>
+public class SurveyQuestion
+{
+    /// <summary>
+    /// Уникальный ID (хэш от QuestionText)
+    /// </summary>
+    public string QuestionId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Полный текст вопроса
+    /// </summary>
+    public string QuestionText { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Тип вопроса
+    /// </summary>
+    public QuestionType Type { get; set; }
+}
